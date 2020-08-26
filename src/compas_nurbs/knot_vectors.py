1584b1b276c69eb3d489400b70bd6c06635db03d
Romana Rust <rust@arch.ethz.ch>
2020-08-26 16:46:07 +0200

    added CurveKnotStyle

--- src/compas_nurbs/knot_vectors.py
+++ src/compas_nurbs/knot_vectors.py
@@ -1,19 +1,21 @@
-"""
-Checkout
-https://github.com/johntfoster/bspline/blob/master/bspline/splinelab.py
-
- knotstyle(opt):
-0 Uniform knots.  Parameter spacing between consecutive knots is 1.0.
-1 Chord length spacing.  Requires degree = 3 with arrCV1 and arrCVn1 specified.
-2 Sqrt (chord length).  Requires degree = 3 with arrCV1 and arrCVn1 specified.
-3 Periodic with uniform spacing.
-4 Periodic with chord length spacing.  Requires an odd degree value.
-5 Periodic with sqrt (chord length) spacing.  Requires an odd degree value.
-
-"""
 import math
 from compas.geometry import Bezier
 from compas.geometry import distance_point_point
+
+
+class CurveKnotStyle(object):
+    """
+    """
+    Uniform = 0
+    Chord = 1
+    ChordSquareRoot = 2
+
+    def __init__(self, style, periodic=False):
+        self.style = style
+        self.periodic = periodic
+
+    # TODO add methods ?
+
 
 def equally_spaced_parameters(num_points):
     """Computes equally spaced parameters for interpolation.
@@ -33,14 +35,50 @@ def equally_spaced_parameters(num_points):
     n = num_points - 1
     return [i / float(n) for i in range(n + 1)]
 
-def chord_lengths(points):
-    """Computes the 
+
+def bezier_spaced_parameters(num_points):
+    """Computes bezier spaced parameters for interpolation.
+
+    Parameters
+    ----------
+    num_points : int
+        Number of points to compute parameters for.
+
+    Returns
+    -------
+    list of float
+        Parameters within the range of [0, 1].
+
+    Notes
+    -----
+    If degree >= 5 this provides a more stable knotvector for 'uniform' knot style.
     """
-    #return np.linalg.norm(points[:-1] - points[1:], axis=1)
+    curve = Bezier([[0.0, 0.0, 0.0], [0.5, 0.0, 0.0], [0.5, 1.0, 0.0], [1., 1., 0]])
+    return [curve.point(t).y for t in equally_spaced_parameters(num_points)]
+
+
+def chord_lengths(points):
+    """Returns the chord lengths.
+
+    Parameters
+    ----------
+    points : list of point
+        Points on the curve.
+
+    Returns
+    -------
+    list of float
+        The n - 1 lengths between the n points.
+
+    Examples
+    --------
+    >>>
+    """
     return [distance_point_point(a, b) for a, b in zip(points[:-1], points[1:])]
 
+
 def chord_spaced_parameters(points):
-    """Computes parameters based on the chord length for interpolation.
+    """Computes parameters based on chord length for interpolation.
 
     Please refer to the Equations 9.4 and 9.5 for chord length parametrization,
     and Equation 9.6 for centripetal method on The NURBS Book (2nd Edition),
@@ -56,17 +94,12 @@ def chord_spaced_parameters(points):
     list of float
         Parameters within the range of [0, 1].
 
-    Note
-    ----
-    This spacing is actually the one to which Rhino refers to as `CurveKnotStyle.Uniform`.
-
     Examples
     --------
     >>>
     """
     cds = chord_lengths(points)
-    d = sum(cds)  # total chord length
-    #uk = np.append(np.append([0], cds), [1.])
+    d = sum(cds)
     uk = [0.] + cds + [1.]
     return [sum(uk[0:i + 1]) / d for i in range(len(points))]
 
@@ -88,34 +121,36 @@ def chord_sqrt_spaced_parameters(points):
     list of float
         Parameters within the range of [0, 1].
 
-    Note
-    ----
-    This spacing is actually the one to which Rhino refers to as `CurveKnotStyle.Uniform`.
-
     Examples
     --------
     >>>
     """
-    #cds = np.sqrt(chord_lengths(points))
     cds = [math.sqrt(v) for v in chord_lengths(points)]
     d = sum(cds)
     uk = [0.] + cds + [1.]
     return [sum(uk[0:i + 1]) / d for i in range(len(points))]
 
 
-
-
-
-def bezier_spaced_parameters(num_points):
-    """
-    """
-    curve = Bezier([[0.0, 0.0, 0.0], [0.5, 0.0, 0.0], [0.5, 1.0, 0.0], [1., 1., 0]])
-    return [curve.point(t).y for t in equally_spaced_parameters(num_points)]
-
-
 def knot_vector_uniform(num_points, degree, periodic=False):
+    """Computes a uniform knot vector.
+
+    Parameters
+    ----------
+    num_points : int
+        Number of points to compute parameters for.
+    degree : int
+        The degree of the curve.
+
+    Returns
+    -------
+    list of float
+        The knot vector in the domain of [0, 1].
+
+    Notes
+    -----
+    Is the same to which Rhino refers to as CurveKnotStyle.Uniform
+
     """
-    """ 
     kv = [0.0 for _ in range(degree + 1)]
     step = 1. / (num_points - degree)
     for i in range(1, num_points - degree):
@@ -125,7 +160,7 @@ def knot_vector_uniform(num_points, degree, periodic=False):
 
 
 def knot_vector_from_params(degree, params, periodic=False):
-    """ Computes a knot vector from parameters using the averaging method.
+    """Computes a knot vector from parameters using the averaging method.
 
     Please refer to the Equation 9.8 on The NURBS Book (2nd Edition), pp.365 for
     details.
@@ -142,7 +177,9 @@ def knot_vector_from_params(degree, params, periodic=False):
     list of float
         The knot vector.
 
-    == from geomdl.fitting.compute_knot_vector
+    Notes
+    -----
+    Is the same as geomdl.fitting.compute_knot_vector
     """
     kv = [0.0 for _ in range(degree + 1)]
     for j in range(1, len(params) - degree):
@@ -153,22 +190,42 @@ def knot_vector_from_params(degree, params, periodic=False):
 
 
 def knot_vector_and_params(points, degree, knot_style, extended=False, periodic=False):
-    """
+    """Returns the knot vector for curve interpolation.
+
+    Parameters
+    ----------
+    points : list of point
+        Points on the curve.
+    degree : int
+        The degree of the curve
+    knot_style : int
+        The knot style: 0 for 'Uniform', 1 for 'Chord', 2 for 'ChordSquareRoot'
+    extended : bool
+        `True` if the knot vector and params should be calculated for interpolation 
+        with end derivatives. Defaults to `False`.
+    periodic : bool
+        `True` if knot vector should be computed for a `closed` curve. Defaults to
+        `False`.
+
+    Returns
+    -------
+    tuple : (knot_vector, parameters)
+        The knot vector and the parameters for the interpolation.
     """
 
-    if knot_style == 'uniform':
-        uk = equally_spaced_parameters(len(points)) if degree <=3 else bezier_spaced_parameters(len(points))
-    elif knot_style == 'chord':
+    if knot_style == CurveKnotStyle.Uniform:
+        uk = equally_spaced_parameters(len(points)) if degree <= 3 else bezier_spaced_parameters(len(points))
+    elif knot_style == CurveKnotStyle.Chord:
         uk = chord_spaced_parameters(points)
-    elif knot_style == 'chord_sqrt':
+    elif knot_style == CurveKnotStyle.ChordSquareRoot:
         uk = chord_sqrt_spaced_parameters(points)
     else:
-        raise NotImplementedError
+        raise ValueError("CurveKnotStyle %d is unknown" % knot_style)
 
-    if extended: # extend parameters for end derivatives estimation
+    if extended:  # extend parameters for end derivatives estimation
         uk = [uk[0], 0.] + uk[1:-1] + [1., uk[-1]]
 
-    if knot_style == 'uniform':
+    if knot_style == CurveKnotStyle.Uniform:
         kv = knot_vector_uniform(len(uk), degree)
     else:
         kv = knot_vector_from_params(degree, uk)
@@ -177,7 +234,17 @@ def knot_vector_and_params(points, degree, knot_style, extended=False, periodic=
 
 
 def normalize_knot_vector(knot_vector):
-    """Normalizes the input knot vector to [0, 1] domain.
+    """Returns a normalized knot vector within the [0, 1] domain.
+
+    Parameters
+    ----------
+    list of float
+        A knot vector
+
+    Returns
+    -------
+    list of float
+        The normalized knot vector.
     """
-    k = float(knot_vector[-1])
-    return [v/k for v in knot_vector]
+    knot_vector = [v - knot_vector[0] for v in knot_vector]
+    return [v/float(knot_vector[-1]) for v in knot_vector]
