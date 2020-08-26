1413f4da651d00e333cde78761caaa8523acc189
Romana Rust <rust@arch.ethz.ch>
2020-08-26 18:17:12 +0200

    update fitting

--- src/compas_nurbs/fitting_numpy.py
+++ src/compas_nurbs/fitting_numpy.py
@@ -4,88 +4,99 @@ from scipy.linalg import lu_factor, lu_solve
 from compas_nurbs.helpers import find_spans
 from compas_nurbs.helpers import basis_functions
 from compas_nurbs.knot_vectors import knot_vector_and_params
+from compas_nurbs.knot_vectors import CurveKnotStyle
 
 
-
+# TODO: estimate derivatives for degree==3
 # https://link.springer.com/content/pdf/10.1007/s003660050038.pdf
 
-
-
-def global_curve_interpolation(points, degree, knot_style='uniform', periodic=False):
-    """Curve interpolation through points.
+def global_curve_interpolation(points, degree, knot_style=0, periodic=False):
+    """Global curve interpolation through points.
 
     Please refer to Algorithm A9.1 on The NURBS Book (2nd Edition), pp.369-370
     for details.
-    
+
     Parameters
     ----------
     points : list of point
+        The list of points on the curve we are looking for.
     degree : int
-        degree of the output parametric curve
-    knotstyle : str, optional
-        Defaults to 'uniform'
-    
+        The degree of the output parametric curve.
+    knotstyle : int, optional
+        The knot style, either 0, 1, or 2 [uniform, chord, or chord_square_root].
+        Defaults to 0, uniform.
+
     Returns
     -------
     tuple (control_points, knot_vector)
-        The interpolated B-Spline curve
-    
 
     Examples
     --------
     >>> degree = 3
     >>> points = [(0., 0., 0.), (3., 4., 0.), (-1., 4., 0.), (-4., 0., 0.), (-4., -3., 0.)]
     >>> control_points, knot_vector = global_curve_interpolation(points, degree)
-    >>> solution = [(0, 0, 0), (7.32, 3.69, 0), (-2.96, 6.68, 0), (-4.49, -0.67, 0), (-4, -3, 0)]
-    >>> allclose(control_points, solution, tol=0.01)
+    >>> solution = [(0, 0, 0), (6.44, 3.72, 0.), (-2.67, 7.5, 0), (-5.11, -2.72, 0), (-4, -3, 0)]
+    >>> allclose(control_points, solution, tol=0.005)
     True
     """
     points = np.array(points)
-
     kv, uk = knot_vector_and_params(points, degree, knot_style, extended=False)
-
     M = coefficient_matrix(degree, kv, uk)
     lu, piv = lu_factor(M)
     return lu_solve((lu, piv), points), kv
 
 
-def estimate_end_derivates():
-    # create a curve where 1st and 2nd point are the same
-    pass
+def global_curve_interpolation_with_end_derivatives(points,
+                                                    degree,
+                                                    start_derivative,
+                                                    end_derivative,
+                                                    knot_style=0,
+                                                    periodic=False):
+    """Global curve interpolation through points with end derivatives specified.
 
+    Please refer to The NURBS Book (2nd Edition), pp. 370-371 for details.
 
-"""
-If unit length tangent vectors Tk are given instead of the Dk, then magnitudes
-uk must be estimated. Setting all ak = d, where d is the total chord length, is
-a reasonable choice.
-"""
+    Parameters
+    ----------
+    points : list of point
+        The list of points on the curve we are looking for.
+    degree : int
+        The degree of the output parametric curve.
+    start_derivative : vector
+        The start derivative of the curve
+    end_derivative : vector
+        The end derivative of the curve
+    knotstyle : int, optional
+        The knot style, either 0, 1, or 2 [uniform, chord, or chord_square_root].
+        Defaults to 0, uniform.
 
+    Returns
+    -------
+    tuple (control_points, knot_vector)
 
-
-
-
-
-def global_curve_interpolation_with_end_derivatives(points, degree,
-                                                       knot_style='uniform',
-                                                       periodic=False,
-                                                       start_derivative=None,
-                                                       end_derivative=None):
+    Examples
+    --------
+    >>> degree = 3
+    >>> points = [(0., 0., 0.), (3., 4., 0.), (-1., 4., 0.), (-4., 0., 0.), (-4., -3., 0.)]
+    >>> start_derivative = [17.75, 10.79, 0.0]
+    >>> end_derivative = [-0.71, -12.62, 0.0]
+    >>> result = global_curve_interpolation_with_end_derivatives(points, degree, start_derivative, end_derivative)
+    >>> solution = [(0, 0, 0), (1.48, 0.9, 0), (4.95, 5.08, 0), (-1.56, 4.87, 0), (-4.72, -0.56, 0), (-3.94, -1.95, 0), (-4, -3, 0)]
+    >>> allclose(result[0], solution, tol=0.005)
+    True
+    """
 
     points = np.array(points)
-
     kv, uk = knot_vector_and_params(points, degree, knot_style, extended=True)
-
-    # Please refer to The NURBS Book (2nd Edition), pp. 370-371 for details.
     M = coefficient_matrix(degree, kv, uk)
+
     M[1][0] = -1.
     M[1][1] = 1.
     M[-2][-2] = -1.
     M[-2][-1] = 1.
 
-    D10 = start_derivative
-    D1n = end_derivative
-    v0 = np.array(D10) * kv[degree+1]/degree
-    vn = np.array(D1n) * (1 - kv[len(kv) - 1 - degree-1])/degree
+    v0 = np.array(start_derivative) * kv[degree+1]/degree
+    vn = np.array(end_derivative) * (1 - kv[len(kv) - 1 - degree-1])/degree
     C = points[:]
     C = np.insert(C, 1, v0, axis=0)
     C = np.insert(C, -1, vn, axis=0)
@@ -102,8 +113,10 @@ def coefficient_matrix(degree, knot_vector, uk):
     Parameters
     ----------
     degree : int
+        The degree of the curve.
     knot_vector : list of float
-    uk : list of float 
+        The knot vector of the curve.
+    uk : list of float
         parameters
 
     Returns
@@ -120,53 +133,73 @@ def coefficient_matrix(degree, knot_vector, uk):
     return np.array(M)
 
 
-def interpolate_curve(points, degree, knot_style='uniform', start_derivative=None, end_derivative=None, periodic=False):
-    """
-    """
+def interpolate_curve(points, degree, knot_style=0, start_derivative=None, end_derivative=None, periodic=False):
+    """Interpolate curve by the specified parameters.
 
-    if degree == 3:
-        # estimate end tangents
-        pass
-    elif start_derivative and end_derivative:
-        return global_curve_interpolation_with_end_derivatives(points, degree,
-                                                       knot_style=knot_style,
-                                                       periodic=periodic,
-                                                       start_derivative=start_derivative,
-                                                       end_derivative=end_derivative)
+    Parameters
+    ----------
+    points : list of point
+        The list of points on the curve we are looking for.
+    degree : int
+        The degree of the output parametric curve.
+    start_derivative : vector, optional
+        The start derivative of the curve. Defaults to ``None``.
+    end_derivative : vector
+        The end derivative of the curve. Defaults to ``None``.
+    knotstyle : int, optional
+        The knot style, either 0, 1, or 2 [uniform, chord, or chord_square_root].
+        Defaults to 0, uniform.
+
+    Returns
+    -------
+    tuple (control_points, knot_vector)
+
+
+    Raises
+    ------
+    ValueError
+        If the knot style is not correct or if only one derivative is passed.
+    """
+    if knot_style not in [CurveKnotStyle.Uniform, CurveKnotStyle.Chord, CurveKnotStyle.ChordSquareRoot]:
+        raise ValueError("Please pass a valid knot style: [0, 1, 2].")
+    if start_derivative and end_derivative:
+        return global_curve_interpolation_with_end_derivatives(points,
+                                                               degree,
+                                                               start_derivative,
+                                                               end_derivative,
+                                                               knot_style,
+                                                               periodic=periodic)
+    elif start_derivative or end_derivative:
+        raise ValueError("Please pass start- AND end derivatives")
     else:
         return global_curve_interpolation(points, degree, knot_style=knot_style, periodic=periodic)
 
 
-
 if __name__ == "__main__":
-    from compas.geometry import allclose # noqa: F401
-    points = [[0.0, 0.0, 0.0], [0.973412, 1.962979, 0.0], [-0.66136, 2.434672, 0.0], [-2.34574, 1.016886, 0.0], [-3.513204, -0.855328, 0.0], [-4.0, -3.0, 0.0]]
-    degree = 7
-    cpts, kv = global_curve_interpolation_with_end_tangents_numpy(points, degree, )
-    solution = [[0.0, 0.0, 0.0], [3.855235, 0.75935299999999994, 0.0], [-0.053739000000000002, 4.2156969999999996, 0.0], [-2.4180000000000001, 3.8224860000000001, 0.0],
-              [-2.3438430000000001, 0.73678500000000002, 0.0], [-2.5536300000000001, -1.20214, 0.0], [-4.5866569999999998, -0.49421999999999999, 0.0], [-4.0, -3.0, 0.0]]
-    print(allclose(cpts, solution))
-
     from compas_nurbs import Curve
-    
-    curve = Curve.from_uniform_knot_style(solution, degree=degree)
-    print(curve.derivatives_at([0, 1], order=1))
+    from compas.geometry import allclose  # noqa: F401
 
-    """
-    [[ 0.          0.          0.        ]
-    [ 3.855235    0.759353    0.        ]
-    [-1.69592187  4.77690428  0.        ]
-    [ 1.52264906  2.74022362  0.        ]
-    [-4.89288482  2.14450472  0.        ]
-    [-1.69889489 -1.41738231  0.        ]
-    [-4.586657   -0.49422    -0.        ]
-    [-4.         -3.          0.        ]]
-    """
+    knot_style = CurveKnotStyle.Uniform
+    points = [[0.0, 0.0, 0.0], [0.973412, 1.962979, 0.0], [-0.66136, 2.434672, 0.0], [-2.34574, 1.016886, 0.0], [-3.513204, -0.855328, 0.0], [-4.0, -3.0, 0.0]]
+    degree = 3
 
-    print(curve.knot_vector)
-    
+    control_points, knot_vector = global_curve_interpolation(points, degree, CurveKnotStyle.Uniform)
+    crv = Curve(control_points, degree, knot_vector)
 
+    start_derivative, end_derivative = [17.75, 10.79, 0.0], [-0.71, -12.62, 0.0]
+    result = global_curve_interpolation_with_end_derivatives(points,
+                                                             degree,
+                                                             start_derivative,
+                                                             end_derivative,
+                                                             knot_style)
 
-    #import doctest
-    #from compas.geometry import allclose  
-    #doctest.testmod(globs=globals())
+    crv = Curve(result[0], degree, result[1])
+    D0, D1 = crv.derivatives_at([0, 1], order=1)
+    assert(allclose(D0, start_derivative))
+    assert(allclose(D1, end_derivative))
+
+    result = interpolate_curve(points, degree, knot_style, end_derivative=start_derivative)
+    crv = Curve(result[0], degree, result[1])
+    D0, D1 = crv.derivatives_at([0, 1], order=1)
+    assert(allclose(D0, start_derivative, tol=0.01))
+    assert(allclose(D1, end_derivative, tol=0.01))
--- tests/tests_fitting.py
+++ tests/tests_fitting.py
@@ -7,6 +7,7 @@ from compas.geometry import allclose
 from compas_nurbs.fitting_numpy import global_curve_interpolation_with_end_derivatives
 from compas_nurbs.fitting_numpy import global_curve_interpolation
 from compas_nurbs.knot_vectors import knot_vector_and_params
+from compas_nurbs.knot_vectors import CurveKnotStyle
 
 from compas_nurbs import Curve
 from compas_nurbs import DATA
@@ -21,7 +22,7 @@ def tests_rhino_compare_params_knot_vectors():
     """Compare curve interpolation with Rhino's curve interpolation.
     """
     degree = 3
-    for knot_style in ['uniform', 'chord', 'chord_sqrt']:
+    for knot_style in [CurveKnotStyle.Uniform, CurveKnotStyle.Chord, CurveKnotStyle.ChordSquareRoot]:
         data = read_json(os.path.join(DATA, "rhino_curve_data_%d_%s.json" % (degree, knot_style)))
         points = np.array(data['points'])
 
@@ -32,12 +33,12 @@ def tests_rhino_compare_params_knot_vectors():
         curve = Curve(data['control_points'], data['degree'], data['knot_vector'])
         D0, Dn = curve.derivatives_at([0, 1], order=1)  # TODO: how to estimate derivatives
 
-        control_points, knot_vector = global_curve_interpolation_with_end_derivatives(points, degree, knot_style=knot_style, periodic=False, start_derivative=D0, end_derivative=Dn)
+        control_points, knot_vector = global_curve_interpolation_with_end_derivatives(points, degree, D0, Dn, knot_style, periodic=False)
 
-        if knot_style != 'chord':
+        if knot_style != CurveKnotStyle.Chord:
             assert(allclose(control_points, data['control_points'], tol=1.4))
 
-    knot_style = 'uniform'
+    knot_style = CurveKnotStyle.Uniform
     for degree in [5, 7, 9]:
         data = read_json(os.path.join(DATA, "rhino_curve_data_%d_%s.json" % (degree, knot_style)))
         points = np.array(data['points'])
@@ -54,35 +55,29 @@ def tests_rhino_compare_params_knot_vectors():
             assert(allclose(control_points, data['control_points'], tol=5.8))
 
 
-def test_end_derivatives():
-
-    data = read_json(os.path.join(DATA, "end_derivatives_data.json"))
-    points = data['points']
-    start_derivative = data['start_derivative']
-    end_derivative = data['end_derivative']
-    knot_style = data['knot_style']
-    degree = data['degree']
-
-    control_points, knot_vector = global_curve_interpolation_with_end_derivatives(points, degree, knot_style, False, start_derivative, end_derivative)
-
-    curve = Curve(control_points, degree, knot_vector)
-    curve.to_json(os.path.join(DATA, "crv.json"))
+def test_interpolation_with_end_derivatives():
+    degree = 3
+    points = [(0., 0., 0.), (3., 4., 0.), (-1., 4., 0.), (-4., 0., 0.), (-4., -3., 0.)]
+    start_derivative = [17.75, 10.79, 0.0]
+    end_derivative = [-0.71, -12.62, 0.0]
+    result = global_curve_interpolation_with_end_derivatives(points, degree, start_derivative, end_derivative)
+    solution = [(0, 0, 0), (1.48, 0.9, 0), (4.95, 5.08, 0), (-1.56, 4.87, 0), (-4.72, -0.56, 0), (-3.94, -1.95, 0), (-4, -3, 0)]
+    crv = Curve(result[0], degree, result[1])
+    D0, D1 = crv.derivatives_at([0, 1], order=1)
+    assert(allclose(D0, start_derivative))
+    assert(allclose(D1, end_derivative))
+    assert(allclose(result[0], solution, tol=0.005))
 
 
 def test_interpolation():
-
-    data = read_json(os.path.join(DATA, "end_derivatives_data.json"))
-    points = data['points']
-    knot_style = data['knot_style']
-    degree = data['degree']
-
-    control_points, knot_vector = global_curve_interpolation(points, degree, knot_style, False)
-
-    curve = Curve(control_points, degree, knot_vector)
-    curve.to_json(os.path.join(DATA, "crv.json"))
+    degree = 3
+    points = [(0., 0., 0.), (3., 4., 0.), (-1., 4., 0.), (-4., 0., 0.), (-4., -3., 0.)]
+    control_points, knot_vector = global_curve_interpolation(points, degree)
+    solution = [(0, 0, 0), (6.44, 3.72, 0.), (-2.67, 7.5, 0), (-5.11, -2.72, 0), (-4, -3, 0)]
+    assert(allclose(control_points, solution, tol=0.005))
 
 
 if __name__ == "__main__":
-    test_end_derivatives()
     test_interpolation()
+    test_interpolation_with_end_derivatives()
     tests_rhino_compare_params_knot_vectors()
