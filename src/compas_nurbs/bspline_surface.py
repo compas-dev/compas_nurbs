593db15f0cd876068a6ce1f7f45287e5f9c10e74
Romana Rust <rust@arch.ethz.ch>
2020-08-29 17:14:36 +0200

    update surface

--- src/compas_nurbs/bspline_surface.py
+++ src/compas_nurbs/bspline_surface.py
@@ -1,5 +1,4 @@
 
-import json
 import compas
 from compas.geometry import Shape
 from .knot_vectors import knot_vector_uniform
@@ -10,50 +9,121 @@ if not compas.IPY:
     from .evaluators_numpy import evaluate_surface
     from .evaluators_numpy import evaluate_surface_derivatives
 else:
+    from .evaluators import create_surface
     from .evaluators import evaluate_surface
     from .evaluators import evaluate_surface_derivatives
 
 
-# TODO: compas.geometry.Primitive: add from_json, to_json
-
+# https://mcneel.github.io/rhino3dm/python/api/Surface.html
 
 class Surface(Shape):
     """
     """
 
-    def __init__(self, control_points, count_u, count_v, degree_u, degree_v, knot_vector_u, knot_vector_v):
-        if count_u * count_v != len(control_points):
-            raise ValueError("u_count * v_count != len(control_points)")
+    def __init__(self, control_points_2d, degree_u, degree_v, knot_vector_u, knot_vector_v):
+        # if count_u * count_v != len(control_points):
+        #    raise ValueError("u_count * v_count != len(control_points)")
         self.degree_u = degree_u
         self.degree_v = degree_v
         self.knot_vector_u = knot_vector_u
         self.knot_vector_v = knot_vector_v
-        self.count_u = count_u
-        self.count_v = count_v
+        #self.count_u = count_u
+        #self.count_v = count_v
+        self.control_points_2d = control_points_2d
+        self.rational = False
+
         if compas.IPY:
-            self.control_points = control_points
-            self.control_points_2d = unflatten(control_points, count_u)
+            #self.control_points_2d = unflatten(control_points, count_u)
+            self._surface = create_surface(control_points_2d, degree_u, degree_v, knot_vector_u, knot_vector_v, rational=False, weights_u=None, weights_v=None)
         else:
-            self.control_points = np.array(control_points)
-            self.control_points_2d = self.control_points.reshape(count_u, count_v, self.control_points[0].shape[0])
+            #self.control_points = np.array(control_points)
+            #self.control_points_2d = self.control_points.reshape(count_u, count_v, self.control_points[0].shape[0])
+            self._surface = self
+
+    @property
+    def domain(self):
+        raise NotImplementedError
+
+    @property
+    def degree(self):
+        raise NotImplementedError
+
+    @property
+    def bounding_box(self):
+        raise NotImplementedError
+
+    # ==========================================================================
+    # constructors
+    # ==========================================================================
 
     @classmethod
-    def from_uniform_knot_style(cls, control_points, count_u, count_v, degree_u, degree_v, periodic='False'):
-        knot_vector_u = knot_vector_uniform(degree_u, count_u, open=True, periodic=periodic)
-        knot_vector_v = knot_vector_uniform(degree_v, count_v, open=True, periodic=periodic)
+    def from_points(cls, points):
+        pass
+
+    @classmethod
+    def from_uniform_knot_style(cls, control_points, count_u, count_v, degree_u, degree_v):
+        knot_vector_u = knot_vector_uniform(degree_u, count_u)
+        knot_vector_v = knot_vector_uniform(degree_v, count_v)
         return cls(control_points, count_u, count_v, degree_u, degree_v, knot_vector_u, knot_vector_v)
 
-    def evaluate_at(self, params_u, params_v, rational=False):
-        return evaluate_surface(self.control_points_2d, self.degree_u, self.degree_v, self.knot_vector_u, self.knot_vector_v, params_u, params_v, rational)
+    # ==========================================================================
+    # evaluate
+    # ==========================================================================
 
-    def tangent_at(self, params):
-        return evaluate_surface_derivatives(self.control_points_2d, self.degree_u, self.degree_v, self.knot_vector_u, self.knot_vector_v, params, order=1)
+    def points_at(self, params):
+        """Evaluates the curve's points at the given parametric positions.
 
-    def to_obj(self):
+        Parameters
+        ----------
+        params: list of tuples (u, v)
+            Evaluation parameters within the curve's domain of [0, 1]
+
+        Returns
+        -------
+        points : list of :class:`Point`
+            Point locations on the surface at the parameters.
+
+        Examples
+        --------
+        >>> 
+        """
+        return evaluate_surface(self._surface, params, self.rational)
+
+    def normals_at(self, params):
         pass
+
+    def frames_at(self, params):
+        pass
+
+    def curvatures_at(self, params):
+        pass
+
+    def isocurve_at(self,):
+        raise NotImplementedError
+
+    # ==========================================================================
+    # operations
+    # ==========================================================================
 
     def transform(self, transformation):
         pass
+
+    def trim(self):
+        raise NotImplementedError
+
+    def split(self):
+        raise NotImplementedError
+
+    def parameters_at(self):
+        raise NotImplementedError
+
+    # ==========================================================================
+    # queries
+    # ==========================================================================
+
+    # ==========================================================================
+    # serialisation
+    # ==========================================================================
 
     @property
     def data(self):
@@ -70,14 +140,8 @@ class Surface(Shape):
     def from_data(cls, data):
         return cls(data['control_points'], data['count_u'], data['count_v'], data['degree_u'], data['degree_v'], data['knot_vector_u'], data['knot_vector_v'])
 
-    def to_json(self, filename):
-        with open(filename, 'w') as f:
-            json.dump(self.data, f, indent=4, sort_keys=True)
-
-    @classmethod
-    def from_json(cls, filename):
-        with open(filename, 'r') as f:
-            return cls.from_data(json.load(f))
+    def to_obj(self):
+        pass
 
 
 if __name__ == "__main__":
