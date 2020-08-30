debac1f25f83780929b160bfd66f3a99102a034e
Romana Rust <rust@arch.ethz.ch>
2020-08-30 23:05:21 +0200

    Update rhino.py

--- src/compas_nurbs/artists/rhino.py
+++ src/compas_nurbs/artists/rhino.py
@@ -2,23 +2,23 @@ def rhino_nurbs_surface_from_surface(surface):
     import Rhino.Geometry as rg
 
     point_count = surface.count_u, surface.count_v
-    points = [rg.Point3d(*p) for p in surface.control_points]
+    #points = [rg.Point3d(*p) for p in surface.control_points]
     degree = surface.degree_u, surface.degree_v
     knots_u = [u for u in surface.knot_vector_u][1:-1]
     knots_v = [v for v in surface.knot_vector_v][1:-1]
     weights = None  # TODO read out
 
-    ns = rg.NurbsSurface.Create(3, weights is not None, degree[0] + 1, degree[1] + 1, point_count[0], point_count[1])
+    ns = rg.NurbsSurface.Create(3, False, degree[0] + 1, degree[1] + 1, point_count[0], point_count[1])
     controlpoints = ns.Points
     index = 0
     for i in range(point_count[0]):
         for j in range(point_count[1]):
+            p = surface.control_points_2d[i][j]
             if weights:
-                cp = rg.ControlPoint(points[index], weights[index])
-                controlpoints.SetControlPoint(i, j, cp)
+                cp = rg.ControlPoint(rg.Point3d(*p), weights[index])
             else:
-                cp = rg.ControlPoint(points[index])
-                controlpoints.SetControlPoint(i, j, cp)
+                cp = rg.ControlPoint(rg.Point3d(*p))
+            controlpoints.SetControlPoint(i, j, cp)
             index += 1
 
     for i in range(ns.KnotsU.Count):
