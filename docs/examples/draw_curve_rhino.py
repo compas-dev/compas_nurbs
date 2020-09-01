a6d226e002aab78a6e8fcab8457bb380cf578cb0
Romana Rust <rust@arch.ethz.ch>
2020-09-01 15:10:48 +0200

    some images

---
+++ docs/examples/draw_curve_rhino.py
@@ -0,0 +1,10 @@
+from compas_nurbs import Curve
+from compas_nurbs.artists.rhino import CurveArtist
+
+control_points = [(0, 0, 0), (3, 4, 0), (-1, 4, 0),
+                  (-4, 0, 0), (-4, -3, 0)]
+curve = Curve(control_points, 3)
+
+artist = CurveArtist(curve)
+artist.draw()
+artist.draw_points()
---
+++ docs/examples/draw_surface_rhino.py
@@ -0,0 +1,13 @@
+from compas_nurbs import Surface
+from compas_nurbs.artists.rhino import SurfaceArtist
+
+control_points = [[[0, 0, 0], [0, 4, 0.], [0, 8, -1]],
+                  [[3, 0, 6], [3, 4, 0.], [3, 8, 0.]],
+                  [[5, 0, 0], [5, 4, 0.], [5, 8, 3.]],
+                  [[8, 0, 0], [8, 4, -3], [8, 8, 0.]]]
+degree = (3, 2)
+
+surface = Surface(control_points, degree)
+
+artist = SurfaceArtist(surface)
+artist.draw()
